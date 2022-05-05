247f50dd6854e21bd2849f82afacd8d7922b58b9
mennaashraff7 <39194503+mennaashraff7@users.noreply.github.com>
2022-05-06 00:06:47 +0200

    ci/cd

--- udagram-frontend/bin/InstallKarmaCli.sh
+++ udagram-frontend/bin/InstallKarmaCli.sh
@@ -1,1 +1,6 @@
 npm install karma-spec-reporter --save-dev --legacy-peer-deps
+# "karma": "~3.1.4",
+#     "karma-chrome-launcher": "~2.2.0",
+#     "karma-coverage-istanbul-reporter": "~2.0.1",
+#     "karma-jasmine": "~1.1.2",
+#     "karma-jasmine-html-reporter": "^0.2.2",
