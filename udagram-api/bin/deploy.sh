b258c750db5f5c8e0f9719be34ceb5083a1af6db
mennaashraff7 <39194503+mennaashraff7@users.noreply.github.com>
2022-05-06 08:10:11 +0200

    modify deploy.sh

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,6 +1,7 @@
 # Deploy to AWS EB
 # cd ./www
 eb init udagram-api --region us-east-1
+eb use udagram-api-dev
 eb setenv DB_PORT=$DB_PORT
 eb setenv PORT=$PORT
 eb setenv POSTGRES_DB=$POSTGRES_DB
@@ -9,5 +10,4 @@ eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
 eb setenv RDS_DIALECT=$RDS_DIALECT
 eb setenv JWT_SECRET=$JWT_SECRET
-eb use udagram-api-dev
 eb deploy udagram-api-dev
